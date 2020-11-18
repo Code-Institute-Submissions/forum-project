ea3f5f2b77b3dd101d8794eb7f40bc34e9041263
flups10 <je.tieleman@gmail.com>
2020-11-18 11:10:02 +0000

    fixed error in python file

--- run.py
+++ run.py
@@ -25,9 +25,7 @@ def home():
     Home page, Gets all topics and categories
     """
     catogories = list(mongo.db.catogories.find())
-    topicsz = list(mongo.db.Topics.find())
-    topics = topicsz.sort('last_edited')
-    print(topics)
+    topics = list(mongo.db.Topics.find())
     return render_template("home.html", topics=topics, catogories=catogories)
 
 
@@ -38,8 +36,6 @@ def catogory(catogory_id):
     """
     catogories = list(mongo.db.catogories.find({"_id": ObjectId(catogory_id)}))
     topics = list(mongo.db.Topics.find())
-    topics.sort('last_edited')
-    print(topics)
     return render_template("catogory.html", catogory=catogories, topics=topics)
 
 
